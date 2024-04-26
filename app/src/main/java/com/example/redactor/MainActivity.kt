2e577e7d3d100ac98c7022754755cb18218be653
rodionrybko <rybkorodion201@mail.ru>
2024-04-26 19:24:57 +0700

    add some disign to main page

--- app/src/main/java/com/example/redactor/MainActivity.kt
+++ app/src/main/java/com/example/redactor/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.redactor
 
+import android.Manifest
+import android.app.Instrumentation.ActivityResult
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var pLauncher: ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,29 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        registerPermissionListner()
+        checkCameraPermission()
+    }
+    private fun checkCameraPermission(){
+        when {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED -> {
+                Toast.makeText(this, "Camera run", Toast.LENGTH_LONG).show()
+            }
+            else ->{
+                pLauncher.launch(Manifest.permission.CAMERA)
+            }
+        }
+    }
+
+    private fun registerPermissionListner(){
+        pLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){
+            if (it){
+                Toast.makeText(this, "Camera run", Toast.LENGTH_LONG).show()
+            }
+            else{
+                Toast.makeText(this, "Permission denied", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
