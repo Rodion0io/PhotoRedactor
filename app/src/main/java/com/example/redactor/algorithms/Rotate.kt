cef12f839836c2046dea2709aeeb3301077a245a
rodionrybko <rybkorodion201@mail.ru>
2024-05-07 01:14:43 +0700

    start algorithm

--- app/src/main/java/com/example/redactor/algorithms/Rotate.kt
+++ app/src/main/java/com/example/redactor/algorithms/Rotate.kt
@@ -1,4 +1,18 @@
 package com.example.redactor.algorithms
 
+import kotlin.math.PI
+
+
 class Rotate {
+    private fun makeRadian(angle: Double):Double{
+        return angle * (PI / 180.0)
+    }
+
+    private fun calculateNewX(angle: Double):Double{
+        return
+    }
+
+    private fun calculateNewY(angle: Double):Double{
+
+    }
 }
