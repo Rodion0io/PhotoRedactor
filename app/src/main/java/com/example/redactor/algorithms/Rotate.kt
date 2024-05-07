bd47b4b2e3bcf4f14a4e006084666ef1834ea0d9
rodionrybko <rybkorodion201@mail.ru>
2024-05-08 00:43:20 +0700

    the second version pf the rotation algorithm

--- app/src/main/java/com/example/redactor/algorithms/Rotate.kt
+++ app/src/main/java/com/example/redactor/algorithms/Rotate.kt
@@ -7,40 +7,42 @@ import kotlin.math.sin
 
 
 class Rotate {
-    private fun makeRadian(angle:Double):Double{
-        return angle * (180.0 / PI)
+    private fun makeRadian(angle: Double): Double {
+        return angle * (PI / 180.0)
     }
 
-    public fun rotatePicture(pict: Bitmap, angle: Double):Bitmap{
-        var radianAngle: Double = makeRadian(angle);
-        val width = pict.width;
-        val height = pict.height;
-        var maxiX = Double.NEGATIVE_INFINITY;
-        var miniX = Double.POSITIVE_INFINITY;
-        var maxiY = Double.NEGATIVE_INFINITY;
-        var miniY = Double.POSITIVE_INFINITY;
+    public fun rotatePicture(pict: Bitmap, angle: Double): Bitmap {
+        val radianAngle = makeRadian(angle)
+        val width = pict.width
+        val height = pict.height
+        val centerX = width / 2.0
+        val centerY = height / 2.0
 
-            for (i in 0 until height){
-                for (j in 0 until width){
-                    val newX = i * cos(radianAngle) + j * sin(radianAngle)
-                    val newY = j * cos(radianAngle) - i * sin(radianAngle)
-                    maxiX = maxOf(newX, maxiX);
-                    maxiY = maxOf(newY, maxiY);
-                    miniX = minOf(newX, miniX);
-                    miniY = minOf(newY, miniY)
+        var maxiX = Double.NEGATIVE_INFINITY
+        var miniX = Double.POSITIVE_INFINITY
+        var maxiY = Double.NEGATIVE_INFINITY
+        var miniY = Double.POSITIVE_INFINITY
+
+        for (i in 0 until height) {
+            for (j in 0 until width) {
+                val newX = (i - centerY) * cos(radianAngle) - (j - centerX) * sin(radianAngle) + centerX
+                val newY = (j - centerX) * cos(radianAngle) + (i - centerY) * sin(radianAngle) + centerY
+                maxiX = maxOf(newX, maxiX)
+                maxiY = maxOf(newY, maxiY)
+                miniX = minOf(newX, miniX)
+                miniY = minOf(newY, miniY)
             }
         }
 
-        var newWidth = maxiX - miniX;
-        var newHeight = maxiY - miniY;
+        val newWidth = (maxiX - miniX).toInt()
+        val newHeight = (maxiY - miniY).toInt()
 
+        val outputBitmap = Bitmap.createBitmap(newWidth, newHeight, pict.config)
 
-        val outputBitmap = Bitmap.createBitmap(newWidth.toInt(), newHeight.toInt(), pict.config)
-
-        for (newX in 0 until newWidth.toInt()) {
-            for (newY in 0 until newHeight.toInt()) {
-                val originalX = ((newX * cos(radianAngle) - newY * sin(radianAngle)) + miniX).toInt()
-                val originalY = ((newY * cos(radianAngle) + newX * sin(radianAngle)) + miniY).toInt()
+        for (newX in 0 until newWidth) {
+            for (newY in 0 until newHeight) {
+                val originalX = ((newX + miniX - centerX) * cos(radianAngle) + (newY + miniY - centerY) * sin(radianAngle) + centerX).toInt()
+                val originalY = ((newY + miniY - centerY) * cos(radianAngle) - (newX + miniX - centerX) * sin(radianAngle) + centerY).toInt()
 
                 if (originalX in 0 until width && originalY in 0 until height) {
                     outputBitmap.setPixel(newX, newY, pict.getPixel(originalX, originalY))
